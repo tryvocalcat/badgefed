3f6ebd03aaef6a14700be32ead03bac3279a8d10
Maho Pacheco <mictlan@gmail.com>
2025-03-13 19:11:21 -0700

    Creation of actor

--- BadgeFed/Services/LocalDbService.cs
+++ BadgeFed/Services/LocalDbService.cs
@@ -62,17 +62,8 @@ public class LocalDbService
 
         var command = connection.CreateCommand();
         command.CommandText = @"
-            INSERT INTO Actor (Id, Name, Summary, AvatarPath, InformationUri, Uri, Domain, PublicKeyPem, PrivateKeyPem)
-            VALUES (@Id, @Name, @Summary, @AvatarPath, @InformationUri, @Uri, @Domain, @PublicKeyPem, @PrivateKeyPem)
-            ON CONFLICT(Id) DO UPDATE SET
-                Name = excluded.Name,
-                Summary = excluded.Summary,
-                AvatarPath = excluded.AvatarPath,
-                InformationUri = excluded.InformationUri,
-                Domain = excluded.Domain,
-                PublicKeyPem = excluded.PublicKeyPem,
-                PrivateKeyPem = excluded.PrivateKeyPem,
-                Uri = excluded.Uri;
+            INSERT INTO Actor (Name, Summary, AvatarPath, InformationUri, Uri, Domain, PublicKeyPem, PrivateKeyPem, Username)
+            VALUES (@Name, @Summary, @AvatarPath, @InformationUri, @Uri, @Domain, @PublicKeyPem, @PrivateKeyPem, @Username);
         ";
 
         command.Parameters.AddWithValue("@Id", actor.Id);
@@ -84,12 +75,13 @@ public class LocalDbService
         command.Parameters.AddWithValue("@PublicKeyPem", actor.PublicKeyPem);
         command.Parameters.AddWithValue("@PrivateKeyPem", actor.PrivateKeyPem);
         command.Parameters.AddWithValue("@Uri", actor.Uri);
+        command.Parameters.AddWithValue("@Username", actor.Username);
 
         command.ExecuteNonQuery();
         transaction.Commit();
     }
 
-    public void DeleteActor(int id)
+    public void DeleteActor(long id)
     {
         using var connection = GetConnection();
         connection.Open();
@@ -99,6 +91,36 @@ public class LocalDbService
         command.Parameters.AddWithValue("@Id", id);
 
         command.ExecuteNonQuery();
+    }
+
+    public List<Actor> GetActors()
+    {
+        var actors = new List<Actor>();
+
+        using var connection = GetConnection();
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Actor";
+
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            actors.Add(new Actor
+            {
+                Id = int.Parse(reader["Id"].ToString()!),
+                FullName = reader["Name"].ToString()!,
+                Summary = reader["Summary"] == DBNull.Value ? string.Empty : reader["Summary"].ToString()!,
+                AvatarPath = reader["AvatarPath"] == DBNull.Value ? null : reader["AvatarPath"].ToString(),
+                InformationUri = reader["InformationUri"] == DBNull.Value ? null : reader["InformationUri"].ToString()!,
+                Domain = reader["Domain"] == DBNull.Value ? null : reader["Domain"].ToString(),
+                PublicKeyPem = reader["PublicKeyPem"] == DBNull.Value ? null : reader["PublicKeyPem"].ToString(),
+                PrivateKeyPem = reader["PrivateKeyPem"] == DBNull.Value ? null : reader["PrivateKeyPem"].ToString(),
+                Username = reader["Username"] == DBNull.Value ? null : reader["Username"].ToString()
+            });
+        }
+
+        return actors;
     }
 
     public Actor GetActorById(int id)
@@ -119,11 +141,11 @@ public class LocalDbService
                 FullName = reader["FullName"].ToString()!,
                 Summary = reader["Summary"] == DBNull.Value ? string.Empty : reader["Summary"].ToString()!,
                 AvatarPath = reader["AvatarPath"] == DBNull.Value ? null : reader["AvatarPath"].ToString(),
-                InformationUri = reader["InformationUri"] == DBNull.Value ? null : new Uri(reader["InformationUri"].ToString()!),
+                InformationUri = reader["InformationUri"] == DBNull.Value ? null : reader["InformationUri"].ToString()!,
                 Domain = reader["Domain"] == DBNull.Value ? null : reader["Domain"].ToString(),
                 PublicKeyPem = reader["PublicKeyPem"] == DBNull.Value ? null : reader["PublicKeyPem"].ToString(),
-                Uri = reader["Uri"] == DBNull.Value ? null : new Uri(reader["Uri"].ToString()!),
-                PrivateKeyPem = reader["PrivateKeyPem"] == DBNull.Value ? null : reader["PrivateKeyPem"].ToString()
+                PrivateKeyPem = reader["PrivateKeyPem"] == DBNull.Value ? null : reader["PrivateKeyPem"].ToString(),
+                Username = reader["Username"] == DBNull.Value ? null : reader["Username"].ToString()
             };
         }
 
@@ -136,7 +158,7 @@ public class LocalDbService
         connection.Open();
 
         var command = connection.CreateCommand();
-        command.CommandText = $"SELECT Id, Name, Summary, AvatarPath, InformationUri, Domain, PublicKeyPem, Uri, PrivateKeyPem FROM Actor WHERE {filter}";
+        command.CommandText = $"SELECT Id, Name, Summary, AvatarPath, InformationUri, Domain, PublicKeyPem, Username, PrivateKeyPem FROM Actor WHERE {filter}";
         
         using var reader = command.ExecuteReader();
         if (reader.Read())
@@ -147,11 +169,11 @@ public class LocalDbService
                 FullName = reader["Name"].ToString()!,
                 Summary = reader["Summary"] == DBNull.Value ? string.Empty : reader["Summary"].ToString()!,
                 AvatarPath = reader["AvatarPath"] == DBNull.Value ? null : reader["AvatarPath"].ToString(),
-                InformationUri = reader["InformationUri"] == DBNull.Value ? null : new Uri(reader["InformationUri"].ToString()!),
+                InformationUri = reader["InformationUri"] == DBNull.Value ? null : reader["InformationUri"].ToString()!,
                 Domain = reader["Domain"] == DBNull.Value ? null : reader["Domain"].ToString(),
                 PublicKeyPem = reader["PublicKeyPem"] == DBNull.Value ? null : reader["PublicKeyPem"].ToString(),
-                Uri = reader["Uri"] == DBNull.Value ? null : new Uri(reader["Uri"].ToString()!),
-                PrivateKeyPem = reader["PrivateKeyPem"] == DBNull.Value ? null : reader["PrivateKeyPem"].ToString()
+                PrivateKeyPem = reader["PrivateKeyPem"] == DBNull.Value ? null : reader["PrivateKeyPem"].ToString(),
+                Username = reader["Username"] == DBNull.Value ? null : reader["Username"].ToString()
             };
         }
 
