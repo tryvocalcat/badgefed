d70acb4729216850119304f8456185d358de6bdd
Maho Pacheco <mictlan@gmail.com>
2025-03-21 22:30:37 -0700

    Updating to admin section

--- BadgeFed/Models/Recipient.cs
+++ BadgeFed/Models/Recipient.cs
@@ -6,33 +6,12 @@ namespace BadgeFed.Models
     {
         public long Id { get; set; }
         
-        public string FullName { get; set; } = null!;
+        public string Name { get; set; } = null!;
         
         public string Email { get; set; } = null!;
         
-        public string? FediverseHandle { get; set; }
-        
-        public string ProfileUri { get; set; } = null!;
+        public string? ProfileUri { get; set; }
 
-        public static string GetAssignedToType(string assignedTo)
-        {
-            if (assignedTo.StartsWith("https://") || assignedTo.StartsWith("http://"))
-            {
-                return "profileuri";
-            }
-            else if (assignedTo.StartsWith("@") && assignedTo.IndexOf('@', 1) > 1)
-            {
-                return "fediverse";
-            }
-            else if (assignedTo.Count(c => c == '@') == 1 && assignedTo.IndexOf('@') > 0)
-            {
-                return "email";
-            }
-            else
-            {
-                return "fullname";
-            }
-
-        }
+        public bool IsActivityPubActor { get; set; } = false;
     }
 }
