1ed75076163c8afc5323792ae62cc9e285b881bf
Maho Pacheco <maho.pacheco@microsoft.com>
2026-02-19 13:23:02 -0800

    Fixing style when sharing

--- src/BadgeFed/Services/ShareHelper.cs
+++ src/BadgeFed/Services/ShareHelper.cs
@@ -12,7 +12,12 @@ public class ShareHelper
         var uri = new Uri(record.IssuedToSubjectUri);
         var mastodonDomain = uri.Host;
 
-        var text = $"I earned a badge! {record.Title} issued by {record.Actor.FullName} on {issueMonth}/{issueYear}";
+        if (mastodonDomain.Contains("linkedin.com"))
+        {
+            return string.Empty;
+        }
+
+        var text = $"I'm very excited to share that I have earned a badge!\n\n{record.Title} issued by {record.Actor.FullName} on {issueMonth}/{issueYear}";
 
         var url = $"{record.NoteId}";
 
@@ -44,9 +49,9 @@ public class ShareHelper
         var issueYear = record.IssuedOn.Year.ToString();
         var issueMonth = record.IssuedOn.Month.ToString();
 
-        var text = $"I'm very excited to share that I have earned a badge! ";
-        text += $"\n {record.Title} issued by {record.Actor.FullName} on {issueMonth}/{issueYear}.";
-        text += $"Check it out at: {url}";
+        var text = $"I'm very excited to share that I have earned a badge!";
+        text += $"\n\n{record.Title} issued by {record.Actor.FullName} on {issueMonth}/{issueYear}.";
+        text += $"\n\nCheck it out at: {url}";
 
         return $"http://www.linkedin.com/shareArticle?url={Uri.EscapeDataString(url)}&text={Uri.EscapeDataString(text)}";
     }
